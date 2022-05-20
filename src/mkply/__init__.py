b291ade5d9f69908429cebe3e9c399132eebdf88
elena-krismer <elena.krismer@hotmail.com>
2022-05-21 01:27:46 +0200

    import all modules

--- src/mkply/__init__.py
+++ src/mkply/__init__.py
@@ -1,3 +1,8 @@
 # read version from installed package
 from importlib.metadata import version
+from os.path import dirname, basename, isfile, join
+import glob
+
 __version__ = version("mkply")
+#modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = ["Listener", "MultiListener", "SingleListener"]#[ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
